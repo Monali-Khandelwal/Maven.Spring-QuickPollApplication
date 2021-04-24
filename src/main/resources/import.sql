610b55767a606a4ea4ce86049062356b2be51f63
Monali <khandelwal.monali@gmail.com>
2021-04-24 17:57:27 -0400

    first commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+
+ -- 1 Favorite color
+insert into poll (poll_id, question) values (1, 'What is your favorite color?');
+insert into option (option_id, option_value, poll_id) values (1, 'Red', 1);
+insert into option (option_id, option_value, poll_id) values (2, 'Green', 1);
+insert into option (option_id, option_value, poll_id) values (3, 'Blue', 1);
+insert into option (option_id, option_value, poll_id) values (4, 'Orange', 1);
+insert into option (option_id, option_value, poll_id) values (5, 'Yellow', 1);
+
